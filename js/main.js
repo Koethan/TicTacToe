4be71756dea9c29c9264286c4ac151d385be73cb
jronnomo <ggronnii@gmail.com>
2022-12-10 18:24:49 -0500

    working skeleton! LETS GOOOOO

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+const squareArr = Array.from(document.querySelectorAll('.square'))
+squareArr.forEach(element => element.addEventListener('click', takeTurn))
+let numTurn = 1
 
+function takeTurn(click){
+    const clickTarget = click.target
+    const selectedSquareID = click.target.id
+    const shapeArea = document.querySelector(`#${selectedSquareID}`).firstElementChild
+    clickTarget.classList.toggle('unselected')
+    if(!clickTarget.classList.contains('unselected')){
+        if(numTurn%2 != 0){
+            console.log(numTurn)
+            shapeArea.innerText = 'X'
+            numTurn++
+        }
+        else{
+            console.log(numTurn)
+            shapeArea.innerText = 'O'
+            numTurn++
+        }
+    }
+    else{
+        numTurn--
+        console.log(numTurn)
+        shapeArea.innerText = ''
+    }
+}
